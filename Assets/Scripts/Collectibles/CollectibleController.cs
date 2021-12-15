657d9e388af7925a7d45d1a47ed49b1e58298254
unknown <gustavoromerosalas@gmail.com>
2021-12-15 19:24:01 -0300

    Initial commit and basic playermovement

---
+++ Assets/Scripts/Collectibles/CollectibleController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CollectibleController : MonoBehaviour
+{
+    public static int coinCount;
+    public GameObject coinCountDisplay;
+    // Update is called once per frame
+    void Update()
+    {
+        coinCountDisplay.GetComponent<Text>().text = "" + coinCount;
+    }
+    
+}
